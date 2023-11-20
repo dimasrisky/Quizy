9417638511914f5dc44ac46e41bef1922300e556
Dimz~ <rizkyalphazee@gmail.com>
2023-11-20 11:09:31 +0700

    refactor(router): Change the router structure to a latest v6 version

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+ } from "react-router-dom";
 import NavLayout from "./layout-route/NavLayout";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Quiz from "./pages/Quiz";
 
+
+// Router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "category",
+        element: <Category />
+      }
+    ]
+  },
+  {
+    path: "quiz/:category/:difficulty/:username",
+    element: <Quiz />
+  }
+])
+
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<NavLayout />}>
-            <Route index element={<Home />} />
-            <Route path="category" element={<Category />} />
-          </Route>
-          <Route path="/quiz/:category/:difficulty/:username" element={<Quiz />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </>
   );
 };
