11b771e8b0e71c8ec49274ddd224e7976ed7b082
dimasrisky <rizkyalphazee@gmail.com>
2024-11-02 16:47:51 +0700

    changing background to webp in loading pages

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 const Loading = () => {
   return (
     <>
-     <div className="flex items-center h-screen w-screen justify-center font-plusjkt bg-[url('/img/bg-primary.jpg')] bg-cover bg-center bg-no-repeat">
+     <div className="flex items-center h-screen w-screen justify-center font-plusjkt bg-[url('/img/bg-primary.webp')] bg-cover bg-center bg-no-repeat">
       <div className="h-[1.5rem] w-[1.5rem] animate-spin rounded-full border-4 border-white border-solid border-t-transparent"></div>
       <p className="ml-2 text-white text-[1rem] font-laleza">Loading...</p>
     </div>
